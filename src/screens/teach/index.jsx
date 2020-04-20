7801ed2a2d1eb974d3f8ffb9c104ef34e96e8a7e
Ralfi Salhon <ralfisalhon@gmail.com>
2020-04-20 01:19:06 -0400

    Made TeachingPage a function

--- src/screens/teach/index.jsx
+++ src/screens/teach/index.jsx
@@ -1,34 +1,27 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 import Line from '../../components/Line';
 
 import Clickable from './components/clickable';
 import CopyCode from '../../components/CopyCode';
 
-class TeachingPage extends Component {
-  render() {
-    return (
+export default function TeachingPage() {
+  const [fill, setFill] = useState(false);
+  return (
+    <div>
+      <h1>Teaching Material</h1>
+      <div className="m-5" />
+      <h3 className="subtitle">This page is in progress...</h3>
+      <Line />
+      <h2 className="text">React Components</h2>
+      <div className="m-20" />
       <div>
-        <h1>Teaching Material</h1>
-        <div className="m-5" />
-        <h3 className="subtitle">This page is in progress...</h3>
-        <Line />
-        <h2 className="text">React Components</h2>
-        <div className="m-20" />
-        <div>
-          <div className="container">
-            <Clickable />
-            <div className="m-20-left" />
-            <CopyCode HTML={'Test Copy HTML'} CSS={'Test Copy CSS'} />
-          </div>
-          <div className="m-10" />
-          <div className="container">
-            <Clickable filled color="coral" />
-          </div>
+        <div className="container">
+          <Clickable filled={fill} onClick={() => setFill(!fill)} color="coral" />
+          <div className="m-20-left" />
+          <CopyCode HTML={'Test Copy HTML'} CSS={'Test Copy CSS'} />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default TeachingPage;
