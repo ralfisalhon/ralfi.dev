b72b1490bee5d4315180f3d3ef0677d4ef491666
Ralfi Salhon <ralfisalhon@gmail.com>
2020-04-24 00:19:53 -0400

    Added sort

--- src/screens/servicenow/index.jsx
+++ src/screens/servicenow/index.jsx
@@ -4,6 +4,40 @@ import Line from '../../components/Line';
 
 export default function ServiceNowPage() {
   const [data, setData] = useState([]);
+  const [sortkey, setSortkey] = useState('title');
+  const [sortdirection, setSortdirection] = useState(false);
+
+  function compare(a, b) {
+    let completedA;
+    let completedB;
+    if (typeof sortkey === 'boolean') {
+      completedA = a[sortkey].toString();
+      completedB = b[sortkey].toString();
+    } else {
+      completedA = a[sortkey];
+      completedB = b[sortkey];
+    }
+
+    let comparison = 0;
+    if (completedA > completedB) {
+      comparison = sortdirection ? 1 : -1;
+    } else if (completedA < completedB) {
+      comparison = sortdirection ? -1 : 1;
+    }
+    return comparison;
+  }
+
+  function sort(newkey) {
+    if (newkey === sortkey) setSortdirection(!sortdirection);
+    setSortkey(newkey);
+
+    data.sort(compare);
+    let result = [];
+    for (let i = 0; i < data.length; i++) {
+      result.push(data[i]);
+    }
+    setData(result);
+  }
 
   const renderTableData = data.map((item) => {
     const { userId, id, title, completed } = item;
@@ -23,7 +57,14 @@ export default function ServiceNowPage() {
     if (data.length === 0) return;
     let header = Object.keys(data[0]);
     return header.map((key, index) => {
-      return <th key={index}>{key.toUpperCase()}</th>;
+      return (
+        <th key={index}>
+          <div className="row">
+            {key.toUpperCase()}
+            <div className="sort" onClick={() => sort(key)} />
+          </div>
+        </th>
+      );
     });
   }
 
