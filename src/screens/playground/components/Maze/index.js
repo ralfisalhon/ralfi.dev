998b3bfe6581dc7f88bdf188158b2b371b013888
Ralfi Salhon <ralfisalhon@gmail.com>
2020-05-10 16:17:10 -0400

    removeWall added

--- src/screens/playground/components/Maze/index.js
+++ src/screens/playground/components/Maze/index.js
@@ -1,18 +1,19 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './maze.css';
-import PropTypes from 'prop-types';
 const squareSize = 20;
 
-const Square = {
-  borders: [true, true, true, true],
-  position: [0, 0],
-  visited: false,
-  text: '.',
-};
+export default function Maze(props) {
+  const { height, width } = props;
+  const numSquaresWide = (width / squareSize) | 0;
+  const numSquaresTall = (height / squareSize) | 0;
+  let squares = make2DArray(numSquaresWide, numSquaresTall);
+  const [maze, setMaze] = useState(printMaze(squares));
 
-class Maze extends Component {
-  printMaze = (arr) => {
+  // useEffect(() => {
+  //   setMaze(printMaze(squares));
+  // }, [squares]);
+
+  function printMaze(arr) {
     if (!arr || arr.length === 0) return;
     let render = [];
     for (let i = 0; i < arr.length; i++) {
@@ -25,54 +26,53 @@ class Maze extends Component {
         className += item.borders[2] ? 'down ' : '';
         className += item.borders[3] ? 'left ' : '';
 
-        row.push(<div className={className}>{item.text}</div>);
+        row.push(
+          <div key={j} className={className}>
+            {item.text}
+          </div>
+        );
       }
-      render.push(<div className="row">{row}</div>);
+      render.push(
+        <div key={i} className="row">
+          {row}
+        </div>
+      );
     }
 
     return render;
-  };
+  }
 
-  make2DArray = (width, height) => {
+  function make2DArray(width, height) {
     let arr = new Array(height);
     for (var i = 0; i < arr.length; i++) {
       arr[i] = new Array(width);
       for (let j = 0; j < arr[i].length; j++) {
-        arr[i][j] = Square;
+        arr[i][j] = {
+          borders: [true, true, true, true],
+          position: [0, 0],
+          visited: false,
+          text: ' ',
+        };
       }
     }
 
     return arr;
-  };
-
-  render() {
-    const { height, width } = this.props;
-    const numSquaresWide = (width / squareSize) | 0;
-    const numSquaresTall = (height / squareSize) | 0;
-    console.log(numSquaresWide, numSquaresTall);
-    let squares = this.make2DArray(numSquaresWide, numSquaresTall);
-
-    console.log('AFTER true', squares);
-    return (
-      <div>
-        {this.printMaze(squares)}
-        tall:{numSquaresTall} wide:{numSquaresWide}
-      </div>
-    );
   }
+
+  function removeWall() {
+    let temp = squares;
+    temp[0][0].borders[1] = false;
+    temp[0][1].borders[3] = false;
+    setMaze(printMaze(temp));
+  }
+
+  return (
+    <div>
+      {maze}
+      tall:{numSquaresTall} wide:{numSquaresWide}
+      <button onClick={() => removeWall()}>
+        <h1>HEY</h1>
+      </button>
+    </div>
+  );
 }
-
-Maze.propTypes = {
-  text: PropTypes.string.isRequired,
-  filled: PropTypes.bool,
-  color: PropTypes.string,
-  press: PropTypes.func,
-};
-
-Maze.defaultProps = {
-  text: 'Maze',
-  filled: false,
-  color: 'var(--dark-color)',
-};
-
-export default Maze;
